e62100339d4f941a62ff685ac1350dfda6020e2b
maradic <marinac62@gmail.com>
2021-07-28 10:35:18 +0200

    added constants

---
+++ WeatherApp/Constants/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  WeatherApp
+//
+//  Created by Marinko on 7/28/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct URL {
+        
+    }
+    
+    static let defaultPlaceholder = "placeholder"
+}
